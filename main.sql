c049cf610d9661479df4d025d3b7c8fb4264143d
Daniel Luftspring <daniel.luftspring@brooklyndata.co>
2023-12-04 10:24:42 -0800

    Fix this (#8)
    * Fix workflow syntax
    
    * Force the checks to run
    
    * Move out of subdirectory
    
    * No subdirectories and delete bad workflow syntax
    
    * Wrong version of the workflow
    
    * Force update of release-gate workflow
    
    * Try marketplace action
    
    * Update workflow to allow merges after certain time
    
    * simplify everything

---
+++ main.sql
@@ -0,0 +1,47 @@
+/* count monthly active users */
+
+with users as (
+    select
+        user_id,
+        min(date) as first_date,
+        max(date) as last_date
+    from
+        `looker-private-demo.ecomm.users`
+    group by
+        1
+    )
+
+, user_actions as (
+    select
+        user_id,
+        date,
+        count(*) as actions
+    from
+        `looker-private-demo.ecomm.user_actions`
+    group by
+        1,2
+    )
+
+, final as (
+    select
+        u.user_id,
+        u.first_date,
+        u.last_date,
+        a.actions
+    from
+        users u
+    left join
+        user_actions a
+    on
+        u.user_id = a.user_id
+    where
+        u.first_date >= '2019-01-01'
+    and
+        u.first_date <= '2019-12-31'
+    and
+        u.last_date >= '2019-01-01'
+    and
+        u.last_date <= '2019-12-31'
+)
+
+select count(*) as monthly_active_users
